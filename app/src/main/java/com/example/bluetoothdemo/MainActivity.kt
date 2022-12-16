f64cb47743e2a838f5ebf174a3af84677641060e
hsuanne <rabbit222nini@gmail.com>
2022-12-16 10:42:31 +0800

    check bluetooth enable

--- app/src/main/java/com/example/bluetoothdemo/MainActivity.kt
+++ app/src/main/java/com/example/bluetoothdemo/MainActivity.kt
@@ -1,11 +1,79 @@
 package com.example.bluetoothdemo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // The BluetoothAdapter represents the device's own Bluetooth adapter (the Bluetooth radio)
+        // and is required for any and all Bluetooth activity
+        val bluetoothManager: BluetoothManager = getSystemService(BluetoothManager::class.java)
+        val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.adapter
+
+        // check if device supports bluetooth
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+            Toast.makeText(this, "device doesn't support Bluetooth", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+
+        // check if bluetooth is enabled
+        val requestBluetooth =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == RESULT_OK) {
+                    //granted
+                    Toast.makeText(this, "bluetooth permission granted", Toast.LENGTH_SHORT).show()
+                    bluetoothAdapter?.enable()
+                } else {
+                    //deny
+                    Toast.makeText(this, "bluetooth permission NOT granted!", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        val requestMultiplePermissions =
+            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+                permissions.entries.forEach {
+                    Log.d("Bluetooth MainActivity", "${it.key} = ${it.value}")
+                }
+                bluetoothAdapter?.enable()
+            }
+
+        if (bluetoothAdapter?.isEnabled == false){
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.BLUETOOTH_CONNECT
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    // for android 12 and higher
+                    requestMultiplePermissions.launch(
+                        arrayOf(
+                            Manifest.permission.BLUETOOTH_SCAN,
+                            Manifest.permission.BLUETOOTH_CONNECT
+                        )
+                    )
+                } else {
+                    // for android 11 and lower
+                    // todo: not yet tested, but should work...
+                    val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                    requestBluetooth.launch(enableBtIntent)
+                }
+                return
+            }
+        }
     }
 }
